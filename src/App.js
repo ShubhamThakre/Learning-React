70f6943268cc0823eaff781a68941827616fd779
Shubham Thakare <sthakare@anant.saama.com>
2019-07-29 15:43:06 +0530

    Udemy Section 3. lect 43. using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import React, { Component } from 'react';
+// Udemy Section 3. lect 43. using the useState() Hook for State Manipulation
+// Discription- This code is moreover use of state as functional management. This is no class based components. Through out this course we are using the class based approched. 
+// Detail hook are given in detial in lect 26
+
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 
-class App extends Component {
-  state = {
+
+const app = props => {
+  
+  const [ personState , setPersonState ] = useState ({
     persons : [
       { name:'Max', age: 32 },
       { name:'Manu', age: 29 }
-    ]
-  }
+    ],
+    otherState: 'some other value'
+  });
 
-  switchNameHandler = () =>{
+
+  console.log(personState);
+  
+  const switchNameHandler = () =>{
     // console.log('ws clicked');
     // dont do this => this.state.person[0].name ='macc'
-    this.setState({
+    setPersonState({
       persons : [
         { name:'Maxu', age: 30 },
         { name:'Man', age: 25 }
-      ]
+      ],
+      otherState: personState.otherState
     })
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>I am React</h1>
-        <p> This is Paragraph..</p>
-        <button onClick={this.switchNameHandler}>Swich Button</button>
-        <Person name =  {this.state.persons[0].name} age ={this.state.persons[0].age} />
-        <Person name =  {this.state.persons[1].name} age ={this.state.persons[1].age}>My Hobbies are racing and dancing.</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>I am React</h1>
+      <p> This is Paragraph..</p>
+      <button onClick={switchNameHandler}>Swich Button</button>
+      <Person name =  {personState.persons[0].name} age ={personState.persons[0].age} />
+      <Person name =  {personState.persons[1].name} age ={personState.persons[1].age}>My Hobbies are racing and dancing.</Person>
+    </div>
+  );
 }
 
 
-export default App;
+
+
+
+
+
+export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = (props) => {
-    console.log('props: ', props)
+    //console.log('props: ', props)
     return (
         <div>
         <p>I'm a person! My name is {props.name} and my age is {props.age}.</p>
