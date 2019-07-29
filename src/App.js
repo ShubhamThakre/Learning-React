52a5bc260bcc34e7e25f9ec0c45e767d6af6acbd
Shubham Thakare <sthakare@anant.saama.com>
2019-07-29 16:42:41 +0530

    Code Commit- 3.45.0 - Udemy Section 3. Lect 45. Passing method reference between the components.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-// Udemy Section 3. Lect 44. Stateless vs Statefull components.
-// Discription-  functions that are having states are statefull components. 
-// Functions that are not having state are stateless components. 
-// Always recommended to have stateless functions. 
-
-// Code Commit- 3.44.0
-
-
-// Following is example of Statefull function.
-
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 
-
-const app = props => {
-  
-  const [ personState , setPersonState ] = useState ({
+class App extends Component {
+  state = {
     persons : [
       { name:'Max', age: 32 },
       { name:'Manu', age: 29 }
-    ],
-    otherState: 'some other value'
-  });
+    ]
+  }
 
-
-  console.log(personState);
-  
-  const switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
     // console.log('ws clicked');
     // dont do this => this.state.person[0].name ='macc'
-    setPersonState({
+    this.setState({
       persons : [
-        { name:'Maxu', age: 30 },
+        { name:newName, age: 30 },
         { name:'Man', age: 25 }
-      ],
-      otherState: personState.otherState
+      ]
     })
-  };
+  }
 
-  return (
-    <div className="App">
-      <h1>I am React</h1>
-      <p> This is Paragraph..</p>
-      <button onClick={switchNameHandler}>Swich Button</button>
-      <Person name =  {personState.persons[0].name} age ={personState.persons[0].age} />
-      <Person name =  {personState.persons[1].name} age ={personState.persons[1].age}>My Hobbies are racing and dancing.</Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>I am React</h1>
+        <p> This is Paragraph..</p>
+        
+        <button 
+          onClick={() => this.switchNameHandler('Max !!')}>Swich Button</button>
+        <Person 
+          name = {this.state.persons[0].name} 
+          age ={this.state.persons[0].age} />
+        <Person 
+          name =  {this.state.persons[1].name} 
+          age ={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max')}>My Hobbies are racing and dancing.</Person>
+      </div>
+    );
+  }
 }
 
 
+export default App;
 
 
 
 
 
-export default app;
+// Udemy Section 3. Lect 45. Passing method reference between the components.
+// Discription-  functions that are having states are statefull components. 
+// Functions that are not having state are stateless components. 
+// Always recommended to have stateless functions. 
+
+// Code Commit- 3.45.0
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-// Code Commit- 3.44.0
-//following is exampole of stateless functions
 const person = (props) => {
     //console.log('props: ', props)
     return (
         <div>
-        <p>I'm a person! My name is {props.name} and my age is {props.age}.</p>
+        <p onClick={props.click}>I'm a person! My name is {props.name} and my age is {props.age}.</p>
         {props.children}
         </div>
     );
 }
 
 export default person;
+
+
+
+
+
+// Code Commit- 3.45.0
